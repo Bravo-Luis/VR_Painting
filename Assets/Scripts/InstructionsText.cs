b854a4e90baa2f7070b87b248853c267f77705fb
HansonYu23 <hansonyu@ucsb.edu>
2024-06-03 15:25:04 -0700

    added mushroom instructions and added an extra soccer instruction

--- Assets/Scripts/InstructionsText.cs
+++ Assets/Scripts/InstructionsText.cs
@@ -34,6 +34,7 @@ public class InstructionsText : FloatEventListener
 
     void Start() {
         fullText.Add("Welcome to VR Painting. Let's learn to paint a soccer ball. Please press the A button on your right controller to continue to the next step.");
+        fullText.Add("To get started with tracing the soccer ball, please select it by pointing your left controller at the soccer ball on the table and pressing the left grip button");
         fullText.Add("First, change your brush color to white by pressing the trigger on your left controller");
         fullText.Add("Position yourself close to the transparent model of the soccer ball by moving near the pedestal");
         fullText.Add("Adjust the size of your brush using the left joystick so that it is easy to make detailed paint strokes");
@@ -41,6 +42,15 @@ public class InstructionsText : FloatEventListener
         fullText.Add("Now change your brush color to black");
         fullText.Add("Lastly, fill in the black portions of the soccer ball while keeping as high an accuracy as possible");
         fullText.Add("Great work! If you want to try a more difficult model you can try tracing the mushroom by selecting it from the table in front of you");
+
+        fullText.Add("To get started with tracing the mushroom, please make sure it is selected by pointing your left controller at the mushroom on the table and pressing the left grip button");
+        fullText.Add("Change your brush color to yellow by pressing the trigger on your left controller");
+        fullText.Add("Make sure the size of your brush is suiable for making detailed paint strokes");
+        fullText.Add("Fill in the yellow areas of the mushroom stem with your right controller while maintaining as high an accuracy as possible");
+        fullText.Add("Next, change your brush color to black and fill in the black spots on the stem");
+        fullText.Add("Great job! The next step is to step change your brush color to red and fill in the red parts of the mushroom cap, make sure to fill in the underside of the cap as well");
+        fullText.Add("Lastly, change your brush color to white and fill in the white areas on the mushroom cap");
+        fullText.Add("Great work! You have finished the guided VR painting tutorial!");
 
         StartCoroutine(TypeText());
     }
